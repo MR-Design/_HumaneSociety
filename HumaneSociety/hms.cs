749e349e830070b0df112de7c06b1f757c4724fd
manzilat <amtulnoorq@yahoo.com>
2018-10-17 15:27:51 -0500

    linked database to the query class and declared iqueryable method to get state  which fixed an error for state

--- HumaneSociety/hms.cs
+++ HumaneSociety/hms.cs
@@ -8,6 +8,84 @@ namespace HumaneSociety
 {
     public static class Query
     {
+        private static HumaineSocietyDataContext database = new HumaineSocietyDataContext();
+
+        internal static void RunEmployeeQueries(Employee employee, string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Client GetClient(string userName, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object GetUserAdoptionStatus(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object RetrieveClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object GetAnimalByID(int iD)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void Adopt(object animal, Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static IQueryable<USState> GetStates()
+        {
+            var requiredData = from x in database.USStates
+                               select x;
+            return requiredData;
         
+    }
+
+        internal static void updateClient(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void AddNewClient(string firstName, string lastName, string username, string password, string email, string streetAddress, int zipCode, int state)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateUsername(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateEmail(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateFirstName(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateAddress(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static void UpdateLastName(Client client)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static object SearchForAnimalByMultipleTraits()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
